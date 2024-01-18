f0bb2714048894d88fcf98c73ce6b98a62e72a9b
STIg89 <andreystig89@gmail.com>
2024-01-18 15:19:22 +0200

    publish

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './js/menu';
+import './js/themes';
+import './js/scroll';
+import './js/slider';
---
+++ src/js/slider.js
@@ -0,0 +1,49 @@
+import Swiper from 'swiper/swiper-bundle';
+import 'swiper/modules/navigation/navigation.scss';
+import 'swiper/modules/pagination/pagination.scss';
+import 'swiper/swiper.scss';
+
+const swiper = new Swiper('.mySwiper', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 3,
+  loop: true,
+  direction: 'horizontal',
+  spaceBetween: 60,
+  speed: 10000,
+  autoplay: {
+    delay: 0,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 30,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    250: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 70
+    }
+  }
+});
